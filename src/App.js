cad0fd1b47a17fdd5f4cf3ac8ee0579febb2b78a
dulminahxxx <dulminahettiarachchi80@gmail.com>
2025-09-20 18:54:19 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+// import Auth from "./pages/Auth/Auth";
+// import Profile from "./pages/Profile/Profile";
+
+function App() {
+   return (
+      <div className="App">
+         <Home/>
+         {/* <Profile /> */}
+         {/* <Auth /> */}
+      </div>
+   );
+}
+
+export default App;
